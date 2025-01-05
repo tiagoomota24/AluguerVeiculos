0d674a4ba507f685f24a5f78f7a92f597d466d03
Tiago Mota <tiago.cmota@hotmail.com>
2025-01-05 15:23:56 +0000

    first commit

---
+++ AluguerVeiculos/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace AluguerVeiculos.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
